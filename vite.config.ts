c5587dec423a175cba9eaf503963a8d531a6556d
SWAYAM SATTWIK PARIDA <swayamsattwik29@gmail.com>
2026-07-17 01:24:53 +0530

    fix: remove terser config to fix vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    }
-  }
 })
